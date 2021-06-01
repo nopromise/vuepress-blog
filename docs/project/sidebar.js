93c837ed22ba0448ef75c7fa39cf3e53ef0bfba6
nopromise <312655057@qq.com>
2021-06-01 14:42:00 +0800

    config css js

---
+++ docs/project/sidebar.js
@@ -0,0 +1,68 @@
+module.exports = [
+	{
+		title: '谷粒学院',
+		collapsable: true,
+		children: [
+			'/project/guli/P1_P5',
+			'/project/guli/P6_P17MybatisPlus',
+			'/project/guli/P18-P21arch-db-projectenv',
+			'/project/guli/P22code-generate',
+			'/project/guli/P23-P25teacher-swagger',
+			'/project/guli/P26-P27common-result',
+			'/project/guli/P28-P32back-teacher',
+			'/project/guli/P33-P36common-log-exception-handle',
+			'/project/guli/P37-P43vscode-ES6',
+			'/project/guli/P44-P52vue',
+			'/project/guli/P53-P56axios-elementUI',
+			'/project/guli/P57-P59nodejs-npm',
+			'/project/guli/P60-babel',
+			'/project/guli/P61-P63module',
+			'/project/guli/P64-P67webpack',
+			'/project/guli/P68-P69front-env-arch',
+			'/project/guli/P70-frontpage-login-issue',
+			'/project/guli/P71-day5',
+			'/project/guli/P72-P73background-login-crossorigion',
+			'/project/guli/P74front-arch-dev',
+			'/project/guli/P75-P76front-teach-manage1',
+			'/project/guli/P77-P82front-teach-manage2',
+			'/project/guli/P83-front-teach-manage-router-issue',
+			'/project/guli/P84-P86day6-alioss',
+			'/project/guli/P87-P90teach-upload',
+			'/project/guli/P91-P92teach-nginx',
+			'/project/guli/P93-P94teach-upload-course',
+			'/project/guli/P95-P96EasyExcel',
+			'/project/guli/P97-P98course-classify',
+			'/project/guli/P99-day7',
+			'/project/guli/P100-course-classify-front',
+			'/project/guli/P100-P103course-classify-front-interface',
+			'/project/guli/P101-P103course-classify-front-interface2',
+			'/project/guli/P104-course-classify-classify',
+			'/project/guli/P105-P106-course-pub-dbtab',
+			'/project/guli/P107-P113course-add',
+			'/project/guli/P114day8',
+			'/project/guli/P115-course-front-add',
+			'/project/guli/P116-P118course-list',
+			'/project/guli/P119-P122course-edit',
+			'/project/guli/P123-P128course-chapter',
+			'/project/guli/P129-P130course-info',
+
+			// '/project/guli/',
+			
+		
+
+
+			// P18-P21前后端分离&数据库设计&搭建项目环境
+			// '/life/life/docker-basic',
+		]
+	},
+	// {
+	// 	title: 'xxx',
+	// 	collapsable: false,
+	// 	children: [
+	// 		'/project/xx/xiamen',
+	// 		// '/life/travel/jenkins-apply',
+	// 	]
+	// }
+	
+
+]
